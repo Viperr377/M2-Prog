2f7bb07280ac88035087954ccebd9ce902ab9ded
Maxim <kakashka921@gmail.com>
2025-12-12 15:22:32 +0100

    Arrays en Lists

---
+++ 04_list/Arrays en Lists/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Arrays_en_Lists;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Run();
+    }
+    static void Run()
+    {
+        string[] characters =
+        {
+            "Pac-Man",
+            "Samus",
+            "Crash Bandicoot",
+            "Spyro the Dragon",
+            "Donkey Kong",
+            "Mario",
+            "Luigi",
+            "Astro",
+            "Solid Snake",
+            "Master Chief",
+            "Kratos",
+            "Sonic the Hedgehog",
+            "Link"
+        };
+
+        List<string> characterList = new List<string>();
+        characterList.Add("Witcher Geralt");
+
+        for (int i = 0; i < characters.Length; i++)
+        {
+            characterList.Add(characters[i]);
+        }
+
+        foreach (string character in characterList)
+        {
+            Console.WriteLine(character);
+        }
+    }
+}
