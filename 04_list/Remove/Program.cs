95c9842a019a14a284895ed60e1d1483073fa69f
Maxim <kakashka921@gmail.com>
2025-12-15 11:52:32 +0100

    Remove

---
+++ 04_list/Remove/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Remove;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Run();
+    }
+    static void Run()
+    {
+        List<double> prijzen = new List<double>
+        {
+            99, 145, 12, 75, 200
+        };
+        foreach (double prijs in prijzen)
+        {
+            Console.WriteLine(prijs);
+        }
+        prijzen.Remove(12);
+    }
+}
