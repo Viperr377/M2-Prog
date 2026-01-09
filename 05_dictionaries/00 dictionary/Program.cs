5a44c19befb5752023ff10e479d7275e0fc10805
Maxim <kakashka921@gmail.com>
2026-01-09 14:25:53 +0100

    00 dictionary

---
+++ 05_dictionaries/00 dictionary/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _00_dictionary;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        run();
+    }
+
+    static void run()
+    {
+        Dictionary<int, string> personen = new Dictionary<int, string>();
+
+        personen.Add(1, "Max");
+        personen.Add(2, "Moritz");
+        personen.Add(3, "Lisa");
+
+        string name1 = personen[1];
+        string name2 = personen[3];
+
+        Console.WriteLine(name1);
+        Console.WriteLine(name2); 
+
+        Console.WriteLine();
+
+        foreach(KeyValuePair<int, string > item in personen)
+        {
+            Console.WriteLine(item.Key + "- " + item.Value);
+        }
+
+        foreach(int kay in personen.Keys)
+        {
+            Console.WriteLine("Key: " + kay);
+        }
+        Console.WriteLine();
+        foreach(string val in personen.Values)
+        {
+            Console.WriteLine("Value: " + val);
+        }
+    }
+}
+
+
