ac353f628a4b6b4f655da3f18d7a21497fee61da
Maxim <kakashka921@gmail.com>
2026-01-16 14:21:36 +0100

    00_file toevoegen

---
+++ 06_fileio/Filelo/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.IO;
+
+class Program
+{
+    static void Run()
+    {
+        Console.WriteLine("File IO project run");
+    }
+    static void Main(string[] args)
+    {
+        Run();
+    }
+}
